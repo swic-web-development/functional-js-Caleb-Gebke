d893abcaf806f58f8f872bd9f5e90d84c2da20fc
Caleb Gebke <caleb.gebke@student.swic.edu>
2025-03-06 12:45:00 -0600

    backpack of ammo example

--- src/index.js
+++ src/index.js
@@ -24,3 +24,28 @@ doSomethingWithNumber(10, divideByTwo) // 5
 doSomethingWithNumber(10, double) // 20
 doSomethingWithNumber(10, triple) // 30
 console.log(doSomethingWithNumber(10, divideByTwo)) // 5
+
+const numbers = [1, 2, 3, 4, 5]
+const doubledNumbers = numbers.map((number) => {
+  return number * 2
+})
+
+console.log(doubledNumbers) // [2, 4, 6, 8, 10]
+
+const backpackFullOfAmmo = [
+  { item: 'bullet', qunatity: 10, priceEa: 0.5 },
+  { item: 'shotgun shells', qunatity: 4, priceEa: 0.5 },
+  { item: 'rockets', qunatity: 1, priceEa: 5.0 },
+  { item: 'energy cell units', qunatity: 20, priceEa: 0.2 },
+]
+
+function calculateOrderCost(order) {
+  return order.quantity * order.priceEa
+}
+
+const totalValue = backpackFullOfAmmo.map((ammo) => {
+  console.log(ammo)
+
+  return 'hi'
+})
+console.log(totalValue)
