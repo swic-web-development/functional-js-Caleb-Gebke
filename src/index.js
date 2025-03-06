71fbfce025c794f2894a1df97dd658c5f4d3ff82
Caleb Gebke <caleb.gebke@student.swic.edu>
2025-03-06 11:34:11 -0600

    function arguments practice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-// biome-ignore lint/complexity/useArrowFunction: <explanation>
-const greet = function () {
-  return 'Hello, World!'
-}
+const greet = () => 'Hello, World!'
 console.log(greet())
+
+// eslint-disable-next-line no-unused-vars
+const calcOrderCost = (order) => order.quantity * order.priceEa
+
+function doSomethingWithNumber(number, callbackFunction) {
+  return callbackFunction(number)
+}
+
+function divideByTwo(number) {
+  return number / 2
+}
+
+function double(number) {
+  return number * 2
+}
+
+function triple(number) {
+  return number * 3
+}
+
+doSomethingWithNumber(10, divideByTwo) // 5
+doSomethingWithNumber(10, double) // 20
+doSomethingWithNumber(10, triple) // 30
+console.log(doSomethingWithNumber(10, divideByTwo)) // 5
