984f083d39722d1173680c57ce1fbd44875d9bf8
Caleb Gebke <caleb.gebke@student.swic.edu>
2025-03-16 23:09:15 -0500

    Coding Challenge 2!

--- src/arrays-practice.js
+++ src/arrays-practice.js
@@ -80,3 +80,66 @@ console.log(totalValue) // 16
 //the reduce method is used to calculate the total value of the items in the backpackFullOfAmmo array
 // the total value is calculated by multiplying the quantity of each item by the price of each item and then adding the result to the total value
 // We saw this in a previous exercise where we used the accumulator to add each total to the next price of each item
+
+const monsters = [
+  // Deals 10-60 damage per bite
+  { name: 'Cacodemon', health: 400, damage: 35 },
+
+  // Deals 15-75 damage per claw/fireball
+  { name: 'Baron of Hell', health: 1000, damage: 45 },
+
+  // Deals 20-200 damage per rocket
+  { name: 'Cyberdemon', health: 4000, damage: 125 },
+
+  // Deals 5-50 damage per bite
+  { name: 'Hell Knight', health: 800, damage: 25 },
+
+  // Deals 10-100 damage per bite
+  { name: 'Imp', health: 200, damage: 20 },
+
+  // Deals 5-25 damage per bite
+  { name: 'Lost Soul', health: 100, damage: 10 },
+
+  // Deals 5-50 damage per bite
+  { name: 'Pinky', health: 300, damage: 15 },
+
+  // Deals 10-100 damage per bite
+  { name: 'Revenant', health: 500, damage: 30 },
+
+  // Deals 5-50 damage per bite
+  { name: 'Spectre', health: 200, damage: 20 },
+
+  // Deals 5-50 damage per bite
+  { name: 'Spider Mastermind', health: 2000, damage: 50 },
+
+  // Deals 5-50 damage per bite
+  { name: 'Vile', health: 1000, damage: 40 },
+
+  // Deals 5-50 damage per bite
+  { name: 'Zombie', health: 100, damage: 5 },
+
+  // Deals 5-50 damage per bite
+  { name: 'Zombieman', health: 200, damage: 10 },
+]
+
+const namesOfMonsters = monsters.map((monster) => {
+  return monster.name
+})
+console.log(namesOfMonsters)
+// The names of all the monsters are returned in an array
+
+const monstersWithOver150Health = monsters.filter((monster) => {
+  return monster.health > 150
+})
+console.log(monstersWithOver150Health)
+// The monsters with health greater than 150 are returned in an array
+
+const totalHealthOfAllMonsters = monsters.reduce((total, monster) => {
+  return total + monster.health
+}, 0)
+console.log(totalHealthOfAllMonsters)
+// 10800
+
+const monstersSortedHighestToLowestDamage = monsters.sort((a, b) => b.health - a.health)
+console.log(monstersSortedHighestToLowestDamage)
+// The monsters are sorted from highest to lowest damage
